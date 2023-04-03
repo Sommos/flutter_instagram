73226ad1b3c3162f6ba142515c1ca3002f350a94
Sommos <Sommosgaming@gmail.com>
2023-04-03 18:44:13 +0100

    added images folder, and basic DefaultTabController UI

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+import '../tabs/feed_view.dart';
+import '../tabs/reels_view.dart';
+import '../tabs/tagged_view.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final List<Widget> tabs = const [
+    // feed
+    Tab(icon: Icon(
+      Icons.image,
+      color: Colors.cyan,
+    ),),
+    // reels
+    Tab(icon: Icon(
+      Icons.video_collection,
+      color: Colors.cyan,
+    ),),
+    // tagged
+    Tab(icon: Icon(
+      Icons.bookmark,
+      color: Colors.cyan,
+    ),),
+  ];
+
+  final List<Widget> tabBarViews = [
+    // feed view
+    const FeedView(),
+    // reels view
+    const ReelsView(),
+    // tagged view
+    const TaggedView(),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(),
+        body: SafeArea(
+          child: Column(
+            children: [
+              // profile
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // following
+                  Column(
+                    children: [
+                      Text("364"),
+                      Text("Following"),
+                    ],
+                  ),
+                  // profile picture
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0, 
+                      vertical: 12.0,
+                    ),
+                    child: Container(
+                      height: 120,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle, 
+                        color: Colors.cyan[400],
+                      ),
+                    ),
+                  ),
+                  // followers
+                  Column(
+                    children: [
+                      Text("30.4k"),
+                      Text("Followers"),
+                    ],
+                  ),
+                ],
+              ),
+              TabBar(
+                tabs: tabs,
+              ),
+        
+              Expanded(
+                child: TabBarView(
+                  children: tabBarViews,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
