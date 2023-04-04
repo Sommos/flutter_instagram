6aac7eecff04dc1321f63f459fa2251c459d4039
Sommos <Sommosgaming@gmail.com>
2023-04-04 03:34:33 +0100

    added font family OldStandardTT

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+        theme: ThemeData(fontFamily: "OldStandardTT"),
         debugShowCheckedModeBanner: false,
-        home: ProfilePage(),
+        home: const ProfilePage(),
     );
   }
 }
